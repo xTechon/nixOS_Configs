461e5e465057b8ac368e3bea0af972f23b563923
W. Daniel Hiromoto <danielhiromoto@outlook.com>
2024-09-18 10:38:34 -0400

    remove xterm

--- hosts/GamingPC/configuration.nix
+++ hosts/GamingPC/configuration.nix
@@ -44,7 +44,7 @@
   services.xserver.enable = true;
   services.displayManager.sddm.enable = true;
   services.desktopManager.plasma6.enable = true;
-
+  services.xserver.excludePackages = with pkgs; [ xterm ];
 
   
 
