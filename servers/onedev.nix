430d5733e743c6b087c6f2f596c412b130b63905
W. Daniel Hiromoto <danielhiromoto@outlook.com>
2024-09-18 14:11:58 -0400

    add onedev server

---
+++ servers/onedev.nix
@@ -0,0 +1,24 @@
+{...}:
+{
+    virtualisation.oci-containers.containers.onedev = {
+        image = "1dev/server";
+        ports = [
+            "6610:6610"
+            "6611:6611"
+        ];
+        volumes = [
+            "/var/run/docker.sock:/var/run/docker.sock"
+            "/var/docker/onedev:/opt/onedev"
+        ];
+        extraOptions = [
+            "--restart=always"
+        ];
+        environment = {
+            INITIAL_USER = "svc-admin";
+            INITIAL_PASSWORD = "12345"; # SET PASSWORD AFTER STARTUP!!!
+            INITIAL_EMAIL = "danielhiromoto@outlook.com";
+            INITIAL_SERVER_URL = "http://onedev.techLab.ddnsfree.com";
+
+        };
+    };
+}
