ccc7a1c3f0c2a3bed207315a93af0ea9ec3c6275
W. Daniel Hiromoto <danielhiromoto@outlook.com>
2025-01-13 07:25:11 -0500

    add a base package set for all hosts

---
+++ modules/base-apps.nix
@@ -0,0 +1,15 @@
+# for packages that should always be installed on every system
+{pkgs, ...}:
+{
+  environment.systemPackages = with pkgs; [
+    vim 
+    wget
+    htop
+    git
+    exfatprogs
+    nil
+    nix-search-cli
+    nixpkgs-fmt
+    openconnect
+  ];
+}
