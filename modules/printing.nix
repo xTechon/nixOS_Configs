bf0f62e792017800b40dabdc8d996f8365b2505f
W. Daniel Hiromoto <danielhiromoto@outlook.com>
2025-01-22 18:31:46 -0500

    Add print module

---
+++ modules/printing.nix
@@ -0,0 +1,18 @@
+{ pkgs, ... }:
+{
+  # Enable CUPS to print documents.
+  services.printing.enable = true;
+  #services.printing.drivers = [ pkgs.epsonscan2.override { withNonFreePlugins = true; withGui = false; } ];
+
+  # Enable scanning
+  hardware.sane.enable = true;
+  hardware.sane.extraBackends = [ (pkgs.epsonscan2.override { withNonFreePlugins = true; withGui = false; }) pkgs.utsushi ];
+
+
+  # enable automatic discovery of network printers
+  services.avahi = {
+    enable = true;
+    nssmdns4 = true;
+    openFirewall = true;
+  };
+}
