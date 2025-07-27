07de379d447692a0095228efaee8de311268c60a
W. Daniel Hiromoto <danielhiromoto@outlook.com>
2025-07-26 22:55:11 -0400

    try different ip for kubes

--- virtualization/kubes-master.nix
+++ virtualization/kubes-master.nix
@@ -3,7 +3,7 @@
 let
   # When using easyCerts=true the IP Address must resolve to the master on creation.
  # So use simply 127.0.0.1 in that case. Otherwise you will have errors like this https://github.com/NixOS/nixpkgs/issues/59364
-  kubeMasterIP = "10.1.1.2";
+  kubeMasterIP = "192.168.1.10";
   kubeMasterHostname = "api.kube";
   kubeMasterAPIServerPort = 6443;
 in
