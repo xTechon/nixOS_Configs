6f65b97d85f4a0bed3a02d0516ce325ae3c2a4d0
W. Daniel Hiromoto <danielhiromoto@outlook.com>
2025-08-02 15:37:55 -0400

    add etcd to k3s

--- virtualization/k3s.nix
+++ virtualization/k3s.nix
@@ -9,6 +9,7 @@
   networking.firewall.allowedUDPPorts = [
     8472 # k3s, flannel: required if using multi-node for inter-node networking
   ];
+  services.etcd.enable = true;
   services.k3s.enable = true;
   services.k3s.role = "server";
   services.k3s.extraFlags = toString [
