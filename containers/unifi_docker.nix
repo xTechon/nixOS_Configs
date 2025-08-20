0525ea76a3c05e622b99f4e7fd3a696bad388291
W. Daniel Hiromoto <danielhiromoto@outlook.com>
2025-08-20 07:47:45 -0400

    update unifi docker container

--- containers/unifi_docker.nix
+++ containers/unifi_docker.nix
@@ -1,4 +1,4 @@
-{ lib, config, ... }:
+{ DOMAIN, lib, config, ... }:
 {
   # make sure containers user/group exists
   imports = [ "../users/containers.nix" ];
@@ -40,12 +40,53 @@
         "5514:5514/udp"
       ];
       log-driver = "journald";
+      /*
       extraOptions = [
         "--network-alias=unifi-network-application"
         "--network=unifi-network-application_unifi-bridge"
       ];
+      */
       dependsOn = [ "unifi-db" ];
     };
-    unifi-db = { };
+    unifi-db = {
+      autoStart = true;
+      image = "docker.io/mongo:3.6";
+      environment = {
+        "PGID" = "1000";
+        "PUID" = "1000";
+        "TZ" = "Etc/UTC";
+      };
+      volumes = [
+        "/var/Containers/unifi/init-mongo.js:/docker-entrypoint-initdb.d/init-mongo.js:rw"
+        "/var/Containers/unifi/unifi-db:/data/db:rw"
+      ];
+      ports = [
+        "27017:27017/tcp"
+      ];
+      log-driver = "journald";
+    };
+  };
+
+  # open ports for the container
+  networking.firewall.allowedTCPPorts = [
+    "8080"
+    "8843"
+    "8880"
+    "6789"
+  ];
+  networking.firewall.allowedUDPPorts = [
+    "3478"
+    "10001"
+    "1900"
+    "5514"
+  ];
+
+  # make a subdomain for the unifi web interface
+  services.nginx.virtualHosts."unifi.${DOMAIN}" = {
+    enableACME = false;
+    forceSSL = false;
+    locations."/" = {
+      proxyPass = "http://localhost:8443/";
+    };
   };
 }
