0b55a0a879b299ecf3a3de54a18ab7d462320b64
W. Daniel Hiromoto <danielhiromoto@outlook.com>
2025-08-20 23:40:12 -0400

    change domain again

--- hosts/nixserver-master/configuration.nix
+++ hosts/nixserver-master/configuration.nix
@@ -20,7 +20,7 @@ in
       ../../package-list.nix
       #../../servers/gitea.nix  
       ../../servers/nginx.nix
-      (import ../../servers/unifi.nix { DOMAIN = "thelab.com"; })
+      (import ../../servers/unifi.nix { DOMAIN = "nixserver-master.com"; })
       #(import ../../virtualization/kubes-networking.nix {interface = "eno1"; })
       ../../virtualization/k3s.nix
       #../../virtualization/kubes-master.nix
