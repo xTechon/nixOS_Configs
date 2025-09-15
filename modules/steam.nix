31e83fada0cc6ec473aa8b8f06e3c83feeb795e6
W. Daniel Hiromoto <danielhiromoto@outlook.com>
2025-09-15 19:37:59 -0400

    Add nintendo joycon support

--- modules/steam.nix
+++ modules/steam.nix
@@ -8,4 +8,12 @@
 
     extest.enable = true;
   };
+  
+  # nintendo joycon support: https://github.com/DanielOgorchock/joycond
+  services.joycond.enable = true;
+  /*
+  To combine two joy-cons into a virtual input device, 
+  press a single trigger on both of them at the same time. 
+  A new uinput device will be created called "Nintendo Switch Combined Joy-Cons".
+  */
 }
