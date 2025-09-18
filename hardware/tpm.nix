5a456601db20ec4a1bcb6b3c9d50939b4d72c609
W. Daniel Hiromoto <danielhiromoto@outlook.com>
2025-09-18 18:44:07 -0400

    Add notes on TPM unlocking

--- hardware/tpm.nix
+++ hardware/tpm.nix
@@ -1,3 +1,14 @@
+/*
+See Guides:
+https://jnsgr.uk/2024/04/nixos-secure-boot-tpm-fde/
+https://oddlama.org/blog/bypassing-disk-encryption-with-tpm2-unlock/#
+Solving security vulnerability:
+https://oddlama.org/blog/bypassing-disk-encryption-with-tpm2-unlock/#crude-implementation-of-pcr15-verification
+https://forge.lel.lol/patrick/nix-config/src/commit/ab2cb2b4d554040ce208fc60624fe729a9d5e32b/modules/ensure-pcr.nix
+*/
+# Commands:
+# sudo systemd-cryptenroll --tpm2-device=list
+# sudo systemd-cryptenroll --tpm2-pcrs=0+2+7+12 --wipe-slot=tpm2 [DEVICE]
 { pkgs, ... }:
 {
   security.tpm2.enable = true;
