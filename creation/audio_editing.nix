ad92f554191462c0ca2e8ec72dbb196509cb44f4
W. Daniel Hiromoto <danielhiromoto@outlook.com>
2026-02-18 22:35:16 -0500

    fix problem with lmms broken install

--- creation/audio_editing.nix
+++ creation/audio_editing.nix
@@ -2,7 +2,7 @@
 {
   environment.systemPackages = with pkgs; [
     polyphone
-    lmms
+    #lmms # causing problems as of 11/7/25 see: https://discourse.nixos.org/t/cant-build-lmms/71529
     reaper
   ];
 }
