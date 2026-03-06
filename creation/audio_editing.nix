2d121632ddd353d0d352f6766a466b04a4359884
W. Daniel Hiromoto <danielhiromoto@outlook.com>
2026-03-05 21:58:10 -0500

    Fix new update

--- creation/audio_editing.nix
+++ creation/audio_editing.nix
@@ -1,7 +1,7 @@
 { pkgs, ... }:
 {
   environment.systemPackages = with pkgs; [
-    polyphone
+    #polyphone
     #lmms # causing problems as of 11/7/25 see: https://discourse.nixos.org/t/cant-build-lmms/71529
     reaper
   ];
